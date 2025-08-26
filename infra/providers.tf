9f46ac51424f76f6c3449ac4d1d1a54799b32c1b
sujeet kumar kushwaha <sujeetkushwaha14@gmail.com>
2025-08-26 07:30:42 +0530

    testd

--- infra/providers.tf
+++ infra/providers.tf
@@ -11,13 +11,13 @@ terraform {
     storage_account_name = "stgbacksk123"
     container_name       = "contnsbackends"
     key                  = "terraform.tfstate"
-    subscription_id = "f0c6c28a-7429-42b3-aefa-63439e20a651"
+    subscription_id      = "f0c6c28a-7429-42b3-aefa-63439e20a651"
   }
 
 }
 
 provider "azurerm" {
   subscription_id = var.subscription_id
-  features { }
+  features {}
 }
 
